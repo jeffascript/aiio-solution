b458af171610a9c04f46ed19b7d42f8e34a3d99c
jeffascript <ofoaro.jeff@gmail.com>
2024-06-22 15:04:47 +0200

    refactor: ♻️ fix cors issues

--- server/server/settings.py
+++ server/server/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-d0z#!fw-$28t)rx%yxxnv(ag1v@qx$14^eralk63bpx4@dv3!a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 
 # Application definition
